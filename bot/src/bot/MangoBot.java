c7318c8a96c31c0cae03d90ea91d227e33356af5
TMGilchrist <telcontar@hotmail.co.uk>
2019-05-05 00:56:51 +0100

    Reworked entirety of bot. Made a bunch of utility functions to separate out code. Greatly refactored the main loop.
    Currently works fairly well as a defensive worker rush EXCEPT the one worker that should remain harvesting does not, as it sees returning workers as "harvesting".

--- bot/src/bot/MangoBot.java
+++ bot/src/bot/MangoBot.java
@@ -31,36 +31,68 @@ public class MangoBot extends AbstractionLayerAI
     private int enemyRaxCount;
     private int workersNear;
     
+    //Number of tiles in which an enemy is considered nearby in relation to player's base and therefore a threat to the workers.
+    //Actual distance is this radius * 2 to account for both axes.
+    private int enemyCheckRadius = 5;
+    
     private Unit myBase;
     
-    //All the player's workers
+    
+    /*--------------------
+     * Unit lists 
+     *------------------- */
+    
+    //All the player's workers.
     private List<Unit> myWorkers = new ArrayList<Unit>();
     
-    //All the enemy workers
+    //Player's workers that are harvesting.
+    private List<Unit> myHarvestingWorkers = new ArrayList<Unit>();
+    
+    
+    //All enemy units in the game.
+    private List<Unit> allEnemyUnits = new ArrayList<Unit>();
+    
+    //All the enemy workers.
     private List<Unit> enemyWorkers = new ArrayList<Unit>();
+    
+    //All the enemy bases.
+    private List<Unit> enemyBases = new ArrayList<Unit>();
+    
+    
+    //Enemy units near base
+    private List<Unit> nearbyEnemyUnits = new ArrayList<Unit>();
     
     //Enemy workers near base
     private List<Unit> nearbyEnemyWorkers = new ArrayList<Unit>();
     
+    
     //Single nearest enemy worker
     private Unit nearestEnemyWorker;
+    
+    
     
     //Current target when workers are attacking nearby enemy workers.
     int attackedWorkerIndex = 0;
     
-    //Number of workers that must alwyas be harvesting.
+    //Number of workers that must always be harvesting.
     int workersAlwaysHarvesting = 1;
     
     
+    
+    //Setup unit types
     public MangoBot(UnitTypeTable utt) 
     {
         super(new AStarPathFinding());
         this.utt = utt;
+        
         worker = utt.getUnitType("Worker");
         base = utt.getUnitType("Base");
         barracks = utt.getUnitType("Barracks");
         
         enemyRaxCount = 0;
+        
+        enemyCheckRadius = enemyCheckRadius * 2;
+        System.out.println("Enemy check radius: " + Integer.toString(enemyCheckRadius));
     }
     
 
@@ -77,7 +109,8 @@ public class MangoBot extends AbstractionLayerAI
     }
    
     
-    @Override
+    //Old version for posterity.
+    /*@Override
     public PlayerAction getAction(int player, GameState gs) 
     {
         PhysicalGameState pgs = gs.getPhysicalGameState();
@@ -174,9 +207,9 @@ public class MangoBot extends AbstractionLayerAI
         	}	  	
         }
         
-        /*
-         * After all units evaluated this tick:
-         * */
+        
+         //After all units evaluated this tick:
+         
        
         
         //Number of targets
@@ -230,9 +263,82 @@ public class MangoBot extends AbstractionLayerAI
         
         return translateActions(player, gs);
     }
+    */
     
     
+    //Main game loop. 
+    //Called every game tick.
+    public PlayerAction getAction(int player, GameState gs) 
+    {
+    	//Get the player and game state.
+        PhysicalGameState pgs = gs.getPhysicalGameState();
+        Player getPlayer = gs.getPlayer(player);
+     
+        //Track all units.
+        TrackUnits(player, pgs);
+        
+        
+        // TODO: issue commands to units.
+        for (Unit unit : pgs.getUnits()) 
+        {                       	
+
+        	//Player actions
+        	if (unit.getPlayer() == player) 
+        	{        		
+        		
+        		if (enemyRaxCount > 0) 
+        		{
+        			//Do macro/defense stuff
+        		}
+        		
+        		else 
+        		{
+        			//Prepare for worker rush
+        			
+                	//Actions from Base.
+                	if (unit.getType() == base)
+                	{   
+                		//Training up to 5 workers. This number should be a variable!
+                		if (myWorkers.size() < 5) 
+                		{
+                    		train(unit, worker);
+                		}
+                	}
+        		}        		
+            	        		
+        	}
+        	
+        }
+        
+		//System.out.print("Size of nearby enemies list: " + Integer.toString(nearbyEnemyUnits.size()) + "\n");   
+		//System.out.println("Number of harvesting workers in main loop: " + Integer.toString(myHarvestingWorkers.size()));
+        
+        //Worker actions.
+		for (Unit worker : myWorkers)
+		{	
+			//Use workers to defend if enemies are nearby.
+			if (nearbyEnemyUnits.size() > 0) 
+			{
+				WorkerDefense(worker);
+				//System.out.print("Workers defending!" + "\n");
+			}
+			
+    		//Send workers to harvest.
+			else if (getAbstractAction(worker) instanceof Harvest != true) 
+    		{
+    			HarvestResource(worker, getPlayer, pgs);
+    		}
+		}
+       
+		
+		//System.out.print(numNearbyEnemyWorkers + "\n");
+        
+		//Game logic. Must be kept for game to work.
+        return translateActions(player, gs);
+    }
     
+    
+    //Check if the unit is an enemy of the inputed player. Quite possibly redundant and not worth using!
     public boolean isEnemy(int unitOwner, int player) 
     {
     	//If unitOwner is not the current player or Gaia
@@ -248,6 +354,7 @@ public class MangoBot extends AbstractionLayerAI
     }
     
     
+    //Should be replaced!
     public void workerBehaviour(Unit worker, Player player, PhysicalGameState pgs) 
     {
             Unit closestBase = null;
@@ -298,6 +405,213 @@ public class MangoBot extends AbstractionLayerAI
 
             }
         }    	
+    
+    
+    /*------------------
+     * Battle Functions
+     * ----------------*/
+    
+    //Behaviour pattern where workers engage nearby enemies and then return to mining.
+    //Currently broken as the one worker that should remain harvesting attacks as it often sees workers on their way to harvest!!!!
+    public void WorkerDefense(Unit worker)
+    {
+    	
+    	//System.out.println("Attacking enemy unit at: " + Integer.toString(nearbyEnemyUnits.get(0).getX()) + ", " + Integer.toString(nearbyEnemyUnits.get(0).getY()));
+		System.out.println("Number of harvesting workers: " + Integer.toString(myHarvestingWorkers.size()));
+    	
+    	Unit targetEnemy = null;
+    	
+    	for (int i = 0; i < nearbyEnemyUnits.size(); i++) 
+    	{
+    		if (nearbyEnemyUnits.get(i) != null) 
+    		{
+    			targetEnemy = nearbyEnemyUnits.get(i);
+    			break;
+    		}
+    	}
+    	
+		//Send non-harvesting workers to attack first nearby enemy
+		if (getAbstractAction(worker) instanceof Harvest != true) 
+		{
+			attack(worker, targetEnemy);
+		} 				
+    	
+		//Send harvesting workers to help if there is at least one worker still harvesting
+		else if (myHarvestingWorkers.size() > 1) 
+    	{
+    		attack(worker, targetEnemy);    		
+    	}
+    }    
+    
+    /*------------------
+     * Utility Functions
+     * ----------------*/
+        
+    //Check for enemies that are nearby.
+    //Search within the checkRadius.
+    public void CheckNearbyEnemies(PhysicalGameState pgs, int checkRadius) 
+    {    	
+    	//Check through enemy list
+    	for (Unit enemyUnit : allEnemyUnits) 
+    	{
+    		int enemyDistance = Math.abs(enemyUnit.getX() - myBase.getX()) + Math.abs(enemyUnit.getY() - myBase.getY());
+    		
+    		//Check for nearby enemy units
+    		if (enemyDistance < checkRadius) 
+    		{
+    			//System.out.println("Nearby enemy added");
+        		//System.out.println("Enemy unit location: " + Integer.toString(enemyUnit.getX()) + ", " + Integer.toString(enemyUnit.getY()));
+        		//System.out.println("Enemy Unit Distance: " + Integer.toString(enemyDistance));
+    			
+    			nearbyEnemyUnits.add(enemyUnit);
+    		}
+    	}    	
+    	    	
+    }
+    
+    //Tracks units in the game adding them to the unit lists. 
+    //Done from the perspective of the selected player.
+    public void TrackUnits(int player, PhysicalGameState pgs) 
+    {
+    	//Clear lists for new game tick.
+        myWorkers = new ArrayList<Unit>();
+        this.myHarvestingWorkers = new ArrayList<Unit>();
+		//System.out.println("Number of harvesting workers in TrackUnits start: " + Integer.toString(myHarvestingWorkers.size()));
+        
+        
+        allEnemyUnits = new ArrayList<Unit>();
+        enemyWorkers = new ArrayList<Unit>();
+        enemyBases = new ArrayList<Unit>();
+        nearbyEnemyWorkers = new ArrayList<Unit>();
+        nearbyEnemyUnits = new ArrayList<Unit>();
+        
+        enemyRaxCount = 0;
+        workersNear = 0;
+        
+    	
+    	//Check all units in the game.
+        for (Unit unit : pgs.getUnits()) 
+        {
+        	
+        	/*-------------------
+        	 * Check Enemy Units
+        	 *-------------------*/        	
+        	
+        	//Catalogue all enemy units that are not dead. Unsure if the last check is necessary.
+        	if ((unit.getPlayer() != player) && (unit.getPlayer() != -1) && (unit.getHitPoints() > 0)) 
+        	{
+        		allEnemyUnits.add(unit);
+        		
+        		//Check workers
+        		if (unit.getType() == worker) 
+        		{
+            		enemyWorkers.add(unit);        			
+        		}
+        		
+        		//Check barracks
+        		else if (unit.getType() == barracks) 
+        		{
+            		enemyRaxCount++;
+        		}
+        		
+        		//Check bases
+        		else if (unit.getType() == base) 
+        		{
+        			enemyBases.add(unit);
+        		}
+        		
+        	}
+        	
+        	
+        	/*-------------------
+        	 * Check Player's Units
+        	 *-------------------*/        	
+        	
+        	//Get player's units.
+        	else if (unit.getPlayer() == player) 
+        	{
+        		//Find player's base. This will need to be updated to handle multiple bases!
+        		if (unit.getType() == base) 
+        		{
+        			myBase = unit;
+        		}
+        		
+            	//Catalogue player's workers.
+            	if (unit.getType() == worker) 
+            	{
+            		myWorkers.add(unit);
+            		
+            		//Check harvesting workers.
+            		if (getAbstractAction(unit) instanceof Harvest == true) 
+            		{
+            			myHarvestingWorkers.add(unit);
+            		}
+            	}  
+        	}     
+        	
+        }
+        
+    	CheckNearbyEnemies(pgs, enemyCheckRadius);    
+        
+    }
+       
+    //Finds the closest resource patch.
+    public Unit GetClosestResource(Unit worker, PhysicalGameState pgs) 
+    {
+        Unit closestResource = null;
+        int closestDistance = 0;
+        
+        //Get closest resources
+        for (Unit u2 : pgs.getUnits()) 
+        {
+            if (u2.getType().isResource) 
+            {
+                int d = Math.abs(u2.getX() - worker.getX()) + Math.abs(u2.getY() - worker.getY());
+                if (closestResource == null || d < closestDistance) 
+                {
+                    closestResource = u2;
+                    closestDistance = d;
+                }
+            }
+        }
+        
+        return closestResource;
+    }
+    
+    //Finds the closest base where resources can be deposited.
+    public Unit GetClosestStockpile(Unit worker, Player player, PhysicalGameState pgs) 
+    {
+        Unit closestBase = null;
+        int closestDistance = 0;
+    	
+        //Get closest stockpile (player's base)
+        for (Unit u2 : pgs.getUnits()) 
+        {
+            if (u2.getType().isStockpile && u2.getPlayer() == player.getID()) 
+            {
+                int d = Math.abs(u2.getX() - worker.getX()) + Math.abs(u2.getY() - worker.getY());
+                if (closestBase == null || d < closestDistance) 
+                {
+                    closestBase = u2;
+                    closestDistance = d;
+                }
+            }
+        }
+        
+        return closestBase;
+        
+    }
+    
+    //Target worker harvests from nearest resource patch and deposits at the nearest base.
+    public void HarvestResource(Unit worker, Player player, PhysicalGameState pgs) 
+    {
+    	Unit closestResource = GetClosestResource(worker, pgs);
+    	Unit closestStockpile = GetClosestStockpile(worker, player, pgs);
+    	
+    	harvest(worker, closestResource, closestStockpile);
+    }
+    
+    
     
     
     @Override
