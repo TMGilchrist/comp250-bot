0ee47395fa6a4ae5af21b6cb30da517c72794b84
TMGilchrist <telcontar@hotmail.co.uk>
2019-05-06 17:47:45 +0100

    Fixed issue where workers were not counterattacking.

--- bot/src/bot/MangoBot.java
+++ bot/src/bot/MangoBot.java
@@ -69,14 +69,12 @@ public class MangoBot extends AbstractionLayerAI
     private List<Unit> forcedHarvestingWorkers = new ArrayList<Unit>();
     
     
-    //Single nearest enemy worker
-    private Unit nearestEnemyWorker;
-    
-    //Current target when workers are attacking nearby enemy workers.
-    int attackedWorkerIndex = 0;
-    
     //Number of workers that must always be harvesting.
-    int minimumHarvesterCount = 1;
+    int minHarvesterCount = 1;
+    
+    //Maximum workers that should be harvesting instead of fighting.
+    int maxHarvesterCount = 1;
+    
     
     //Maximum number of workers to train.
     int maxWorkerTrain = 50;
@@ -111,164 +109,7 @@ public class MangoBot extends AbstractionLayerAI
         return new MangoBot(utt);
     }
    
-    
-    //Old version for posterity.
-    /*@Override
-    public PlayerAction getAction(int player, GameState gs) 
-    {
-        PhysicalGameState pgs = gs.getPhysicalGameState();
-        Player getPlayer = gs.getPlayer(player);
-        
-        //Reset rax count.
-        enemyRaxCount = 0;
-        workersNear = 0;
-        
-        System.out.print("Get Action called:");
-        myWorkers.clear();
-        enemyWorkers.clear();
-        nearbyEnemyWorkers.clear();
-        
-        // TODO: issue commands to units
-        for (Unit unit : pgs.getUnits()) 
-        {
-        	
-        	//Get player's units
-        	if (unit.getPlayer() == player) 
-        	{
-        		if (unit.getType() == base) 
-        		{
-        			myBase = unit;
-        		}
-        		
-            	//Find workers
-            	if (unit.getType() == worker) 
-            	{
-            		//Add worker
-            		myWorkers.add(unit);
-            	}  
-        	}
-        	
-        	//Check how many barracks the enemy has built.
-        	if ((unit.getType() == barracks) && (isEnemy(unit.getPlayer(), player))) 
-        	{
-        		enemyRaxCount++;
-        	}
-        	
-        	//Catalogue enemy workers
-        	if ((unit.getType() == worker) && (isEnemy(unit.getPlayer(), player))) 
-        	{
-        		//Get all enemy workers
-        		enemyWorkers.add(unit);
-        		
-        		//Check for nearby enemy workers
-        		if ((unit.getX() - myBase.getX() < 5) || (unit.getY() - myBase.getY() < 5)) 
-        		{
-        			nearbyEnemyWorkers.add(unit);
-        			nearestEnemyWorker = unit; //Test with just the nearest worker at a time
-        		}
-        		
-        		//Remove any workers who are no longer nearby from the nearby workers list
-        		else 
-        		{
-        			for (int i = 0; i < nearbyEnemyWorkers.size(); i++) 
-        			{
-        				if (nearbyEnemyWorkers.get(i) == unit) 
-        				{
-        					nearbyEnemyWorkers.remove(i);
-        				}
-        			}      			
-        		}
-        	}
-        	
-
-        	//Player actions
-        	if (unit.getPlayer() == player) 
-        	{
-            	//Find workers
-            	if (unit.getType() == worker) 
-            	{
-            		//workerBehaviour(unit, getPlayer, pgs);
-            	}  
-        		
-        		if (enemyRaxCount > 0) 
-        		{
-        			//Do macro/defense stuff
-        		}
-        		
-        		else 
-        		{
-        			//Prepare for worker rush
-        			
-                	//Train worker from player's base
-                	if (unit.getType() == base)
-                	{        		
-                		train(unit, worker);
-                	}
-        		}
-        		
-            	     		
-        	}	  	
-        }
-        
-        
-         //After all units evaluated this tick:
-         
-       
-        
-        //Number of targets
-        int numNearbyEnemyWorkers = nearbyEnemyWorkers.size();
-        
-        //Workers that will always continue harvesting
-        int forcedHarvestingWorkers = 0;
-        
-        //Each worker, if not attacking, attacks an enemy worker that is nearby.
-		for (Unit worker : myWorkers)
-		{			
-			AbstractAction currentWorkerAction = getAbstractAction(worker);
-			
-			//If harvesting
-			if ((currentWorkerAction instanceof Harvest == true) && (forcedHarvestingWorkers < workersAlwaysHarvesting)) 
-			{
-				forcedHarvestingWorkers++;
-			}
-			
-			
-			//This worker's current action
-			//AbstractAction currentWorkerAction = getAbstractAction(worker);
-			
-			//If a worker is not attacking and there are available targets, assign it to attack the target
-			else if ((currentWorkerAction instanceof Attack == false) && (attackedWorkerIndex < numNearbyEnemyWorkers)) 
-			{
-				//attack(worker, nearbyEnemyWorkers.get(attackedWorkerIndex));
-				attack(worker, nearestEnemyWorker);
-				
-				//Ensure we do not go outside index range.
-				if (attackedWorkerIndex + 1 < numNearbyEnemyWorkers) 
-				{
-					//Move on to next target
-					attackedWorkerIndex++;
-				}
-				
-				else 
-				{
-					attackedWorkerIndex = 0;
-				}
-			}	
-
-			else 
-			{
-				workerBehaviour(worker, getPlayer, pgs);				
-			}
-			
-		}
-        
-		System.out.print(numNearbyEnemyWorkers + "\n");
-        
-        return translateActions(player, gs);
-    }
-    */
-    
-    
+   
     //Main game loop. 
     //Called every game tick.
     public PlayerAction getAction(int player, GameState gs) 
@@ -279,6 +120,7 @@ public class MangoBot extends AbstractionLayerAI
      
         //Track all units.
         TrackUnits(player, pgs);
+        //System.out.println("I AM PLAYER: " + player);
         
         
         // TODO: issue commands to units.
@@ -319,8 +161,6 @@ public class MangoBot extends AbstractionLayerAI
 			workerBehaviour(worker, getPlayer, pgs);
 		}
        
-		
-		//System.out.print(numNearbyEnemyWorkers + "\n");
         
 		//Game logic. Must be kept for game to work.
         return translateActions(player, gs);
@@ -346,17 +186,23 @@ public class MangoBot extends AbstractionLayerAI
     //Should be replaced!
     public void workerBehaviour(Unit worker, Player player, PhysicalGameState pgs) 
     {
-		if (forcedHarvestingWorkers.size() < minimumHarvesterCount) 
+    	//Force workers to harvest.
+		if (forcedHarvestingWorkers.size() < minHarvesterCount) 
 		{
 			HarvestResource(worker, player, pgs);
 			forcedHarvestingWorkers.add(worker);
 		}
 		
 		//Use workers to defend if enemies are nearby.
-		if (nearbyEnemyUnits.size() > 0) 
+		else if (nearbyEnemyUnits.size() > 0) 
 		{
 			WorkerDefense(worker);
-			//System.out.print("Workers defending!" + "\n");
+		}
+		
+		//Attack with non-essential workers.
+		else if ((myHarvestingWorkers.size() >= maxHarvesterCount) && (forcedHarvestingWorkers.contains(worker) == false)) 
+		{
+			BasicOffense(worker);
 		}
 		
 		//Send workers to harvest.
@@ -364,7 +210,8 @@ public class MangoBot extends AbstractionLayerAI
 		{
 			HarvestResource(worker, player, pgs);
 		}
-        }    	
+		
+    }    	
     
     
     /*------------------
@@ -374,22 +221,7 @@ public class MangoBot extends AbstractionLayerAI
     //Behaviour pattern where workers engage nearby enemies and then return to mining.
     //Currently broken as the one worker that should remain harvesting attacks as it often sees workers on their way to harvest!!!!
     public void WorkerDefense(Unit worker)
-    {
-    	
-    	//System.out.println("Attacking enemy unit at: " + Integer.toString(nearbyEnemyUnits.get(0).getX()) + ", " + Integer.toString(nearbyEnemyUnits.get(0).getY()));
-		System.out.println("Number of harvesting workers: " + Integer.toString(myHarvestingWorkers.size()));
-    	
-    	Unit targetEnemy = null;
-    	
-    	for (int i = 0; i < nearbyEnemyUnits.size(); i++) 
-    	{
-    		if (nearbyEnemyUnits.get(i) != null) 
-    		{
-    			targetEnemy = nearbyEnemyUnits.get(i);
-    			break;
-    		}
-    	}
-    	
+    {    	
 		//Send non-harvesting workers to attack first nearby enemy
 		if (getAbstractAction(worker) instanceof Harvest != true) 
 		{
@@ -404,20 +236,34 @@ public class MangoBot extends AbstractionLayerAI
 			BasicOffense(worker);
     	}
     }    
+
     
-    
+    /**
+     * Basic combat function. Unit attacks the closest valid enemy.
+     * 
+     * @param unit <Unit>: The unit that will look for enemies to attack.
+     */
     public void BasicOffense(Unit unit) 
     {
-    	//Find the closest enemy to this unit and attack it.
-    	Unit target = getClosestEnemy(unit);
-    	
-    	if (target != null) 
+    	if (allEnemyUnits.size() > 0) 
     	{
-        	attack(unit, target);
+        	//Find the closest enemy to this unit and attack it.
+        	Unit target = getClosestEnemy(unit);
+        	
+        	if (target != null) 
+        	{
+            	attack(unit, target);
+        	}	
     	}
 
     }
     
+    /**
+     * Gets the closest enemy unit.
+     * 
+     * @param unit <Unit>: Unit checking for closest enemy.
+     * @return <Unit> closestEnemy: The closest enemy unit.
+     */
     public Unit getClosestEnemy(Unit unit) 
     {
     	int closestEnemyDistance = 100000;
@@ -434,7 +280,8 @@ public class MangoBot extends AbstractionLayerAI
     			closestEnemy = enemyUnit;    			
 			}
     	}    	
-    	
+
+    	//System.out.println("Closet enemy found. Player: " + closestEnemy.getPlayer());
     	return closestEnemy;
     }
     
@@ -476,8 +323,7 @@ public class MangoBot extends AbstractionLayerAI
     {
     	//Clear lists for new game tick.
         myWorkers = new ArrayList<Unit>();
-        this.myHarvestingWorkers = new ArrayList<Unit>();
-		//System.out.println("Number of harvesting workers in TrackUnits start: " + Integer.toString(myHarvestingWorkers.size()));
+        myHarvestingWorkers = new ArrayList<Unit>();
         
         
         allEnemyUnits = new ArrayList<Unit>();
@@ -485,7 +331,7 @@ public class MangoBot extends AbstractionLayerAI
         enemyBases = new ArrayList<Unit>();
         nearbyEnemyWorkers = new ArrayList<Unit>();
         nearbyEnemyUnits = new ArrayList<Unit>();
-        forcedHarvestingWorkers = new ArrayList<Unit>();
+        //forcedHarvestingWorkers = new ArrayList<Unit>();
         
         enemyRaxCount = 0;
         workersNear = 0;
